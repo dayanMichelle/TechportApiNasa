55529289029432104bdfa354c505afe2ff2057e4
dayanMichelle <dayaara6@gmail.com>
2021-09-04 17:06:51 -0400

    Anadido el titulo al azar

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,27 @@
-const info = document.querySelector("#info");
+
 const API_KEY = "Nws0rpOs4emEzL10p8NB2PupcGTTnQd7dqlcOetU";
-const ids_ = document.querySelector("#ids");
 //obtener info segun id
+
 const request = new XMLHttpRequest();
 const ids = new XMLHttpRequest();
 const idsArray = [];
 
+//constantes de html
+const info = document.querySelector("#info");
+const title = document.querySelector("#title");
+
+title.classList.add("title");
+
 function Buscar(id) {
   request.open(
     "GET",
-    "https://api.nasa.gov/techport/api/projects/"+id+"?api_key=" + API_KEY,
+    "https://api.nasa.gov/techport/api/projects/" + id + "?api_key=" + API_KEY,
     true
   );
   request.onload = function () {
     if (request.status >= 200 && request.status < 400) {
       const data = JSON.parse(this.response);
-      info.innerHTML = data.project.title;
+      title.innerHTML = data.project.title;
     } else {
       info.innerHTML = "Ha ocurrido un error con código " + request.status;
     }
@@ -41,10 +47,11 @@ ids.onload = function () {
 };
 ids.send();
 // numero aleatorio
-var num = Math.floor(Math.random() * ((12779+1) - 0) + 0);
-function n()
-{for(var i = 0; i<=12779; i++){
-  var y = idsArray[num]
-}
-  Buscar(y)
+function n() {
+  var num = Math.floor(Math.random() * (12779 + 1 - 0) + 0);
+
+  for (var i = 0; i <= 12779; i++) {
+    var y = idsArray[num];
+  }
+  Buscar(y);
 }
