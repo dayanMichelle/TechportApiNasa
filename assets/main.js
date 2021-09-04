4784b8f7feb271c9f0abb9d0f8be1b23d75ee200
dayanMichelle <dayaara6@gmail.com>
2021-09-04 15:55:33 -0400

    generando proyecto aleatorio

--- assets/main.js
+++ assets/main.js
@@ -4,41 +4,47 @@ const ids_ = document.querySelector("#ids");
 //obtener info segun id
 const request = new XMLHttpRequest();
 const ids = new XMLHttpRequest();
-const idsArray = []
-       
-request.open(
-  "GET",
-  "https://api.nasa.gov/techport/api/projects/93071?api_key="+API_KEY,
-  true
-);
-request.onload = function () {
-  if (request.status >= 200 && request.status < 400) {
-    const data = JSON.parse(this.response);
-    info.innerHTML=(data.project.title);
+const idsArray = [];
 
-  }else {
-    info.innerHTML = ('Ha ocurrido un error con código ' + request.status);
+function Buscar(id) {
+  request.open(
+    "GET",
+    "https://api.nasa.gov/techport/api/projects/"+id+"?api_key=" + API_KEY,
+    true
+  );
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      const data = JSON.parse(this.response);
+      info.innerHTML = data.project.title;
+    } else {
+      info.innerHTML = "Ha ocurrido un error con código " + request.status;
+    }
   };
+  request.send();
 }
-request.send();
 
 //obtener ids
 ids.open(
   "GET",
-  "https://api.nasa.gov/techport/api/projects?api_key="+API_KEY,
+  "https://api.nasa.gov/techport/api/projects?api_key=" + API_KEY,
   true
 );
 ids.onload = function () {
-  if (request.status >= 200 && ids.status < 400) {
-  const dataIds = JSON.parse(this.response);
-for(var i = 0 ; i<= dataIds.totalCount; i++){
-
-      idsArray.push(dataIds.projects[i].projectId) ;
- 
-}
-
-  }else {
-    ids_.innerHTML = ('Ha ocurrido un error conc ódigo ' + request.status);
-   };
+  if (ids.status >= 200 && ids.status < 400) {
+    const dataIds = JSON.parse(this.response);
+    for (var i = 0; i <= dataIds.totalCount; i++) {
+      idsArray.push(dataIds.projects[i].projectId);
+    }
+  } else {
+    ids_.innerHTML = "Ha ocurrido un error conc ódigo " + request.status;
   }
+};
 ids.send();
+// numero aleatorio
+var num = Math.floor(Math.random() * ((12779+1) - 0) + 0);
+function n()
+{for(var i = 0; i<=12779; i++){
+  var y = idsArray[num]
+}
+  Buscar(y)
+}
