59b360056eab373dd4818be7c6a925873c6f536f
dayanMichelle <dayaara6@gmail.com>
2021-09-04 15:23:07 -0400

    obtuve los ids

--- assets/main.js
+++ assets/main.js
@@ -1,20 +1,44 @@
 const info = document.querySelector("#info");
-        const API_KEY = "Nws0rpOs4emEzL10p8NB2PupcGTTnQd7dqlcOetU";
-        const request = new XMLHttpRequest();
+const API_KEY = "Nws0rpOs4emEzL10p8NB2PupcGTTnQd7dqlcOetU";
+const ids_ = document.querySelector("#ids");
+//obtener info segun id
+const request = new XMLHttpRequest();
+const ids = new XMLHttpRequest();
+const idsArray = []
+       
+request.open(
+  "GET",
+  "https://api.nasa.gov/techport/api/projects/93071?api_key="+API_KEY,
+  true
+);
+request.onload = function () {
+  if (request.status >= 200 && request.status < 400) {
+    const data = JSON.parse(this.response);
+    info.innerHTML=(data.project.title);
 
-        request.open(
-          "GET",
-          "https://api.nasa.gov/techport/api/projects/93071?api_key="+API_KEY,
-          true
-        );
-        request.onload = function () {
-          if (request.status >= 200 && request.status < 400) {
-          const data = JSON.parse(this.response);
+  }else {
+    info.innerHTML = ('Ha ocurrido un error con código ' + request.status);
+  };
+}
+request.send();
 
-            info.innerHTML=(data.project.title);
+//obtener ids
+ids.open(
+  "GET",
+  "https://api.nasa.gov/techport/api/projects?api_key="+API_KEY,
+  true
+);
+ids.onload = function () {
+  if (request.status >= 200 && ids.status < 400) {
+  const dataIds = JSON.parse(this.response);
+for(var i = 0 ; i<= dataIds.totalCount; i++){
 
-          }else {
-           info.innerHTML = ('Ha ocurrido un error conc ódigo ' + request.status);
-           };
-          }
-        request.send();
+      idsArray.push(dataIds.projects[i].projectId) ;
+ 
+}
+
+  }else {
+    ids_.innerHTML = ('Ha ocurrido un error conc ódigo ' + request.status);
+   };
+  }
+ids.send();
