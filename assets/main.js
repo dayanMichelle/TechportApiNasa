47bc1ac9d42dc193ea0bd83e5c7e4ad63ccf76ae
dayanMichelle <dayaara6@gmail.com>
2021-09-05 15:28:19 -0300

    Informacion obtenida y organizada

--- assets/main.js
+++ assets/main.js
@@ -9,8 +9,16 @@ const idsArray = [];
 //constantes de html
 const info = document.querySelector("#info");
 const title = document.querySelector("#title");
-
+const subtitle = document.querySelector("#subtitle");
+const definition = document.querySelector("#definition");
+const exampleTechnologies = document.querySelector("#exampleTechnologies")
+const description = document.querySelector("#description")
+const  statesWithWork = document.querySelector("#statesWithWork");
+const startDateString = document.querySelector("#startDateString")
+const lastUpdated = document.querySelector("#lastUpdated")
+//añadiendo clases
 title.classList.add("title");
+subtitle.classList.add("subtitle")
 
 function Buscar(id) {
   request.open(
@@ -22,6 +30,13 @@ function Buscar(id) {
     if (request.status >= 200 && request.status < 400) {
       const data = JSON.parse(this.response);
       title.innerHTML = data.project.title;
+      subtitle.innerHTML = data.project.primaryTaxonomyNodes[0].title;
+      definition.innerHTML = data.project.primaryTaxonomyNodes[0].definition;
+      exampleTechnologies.innerHTML = "<p class='sub_title'>EXAMPLES:</p>"+ data.project.primaryTaxonomyNodes[0].exampleTechnologies;
+      description.innerHTML = "<p class='sub_title'>DESCRIPTION: </p>" + data.project.description;
+      statesWithWork.innerHTML = "<p class='sub_title'>Citys with work </p>"+ data.project.statesWithWork[0].name;
+      startDateString.innerHTML = "<p class='sub_title'>Date start: </p>" + data.project.startDateString;
+      lastUpdated.innerHTML = "<p class='sub_title'>Last update: </p>" + data.project.lastUpdated;
     } else {
       info.innerHTML = "Ha ocurrido un error con código " + request.status;
     }
@@ -54,4 +69,24 @@ function n() {
     var y = idsArray[num];
   }
   Buscar(y);
+}
+
+
+/*slider */
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
 }
